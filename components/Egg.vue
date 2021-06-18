3e85f5638a55915b33cd4c0d3c3591681d44569a
Carmen L. Seva <clavinia.s@gmail.com>
2021-06-18 14:22:27 +0200

    Random animation set for initial egg

--- components/Egg.vue
+++ components/Egg.vue
@@ -3,8 +3,13 @@
         <div class="row justify-content-center text-center">
             <div class="col col-md-10 col-lg-8 col-xl-6">
                 <h1 class="mb-3">Oh, it's an egg!</h1>
-                <h2 class="mb-3">I wonder which animal is inside.</h2>
-                <img class="ini-egg p-3 mt-2 mb-2" src="~/assets/img/egg-test.svg" alt="">
+                <h2 class="mb-4">I wonder which animal is inside.</h2>
+                <div class="egg-container p-3 mt-2 mb-2" ref="eggcontainer">
+                    <img 
+                        :class="[ animate ? 'animateEgg' : '', 'ini-egg']"
+                        :data-anicount="animationCount" 
+                        src="~/assets/img/egg_whatisinside.svg" alt="">    
+                </div>
 
                 <!-- Check the Egg logic -->
                 <h3 class="mt-3 mb-3">Maybe it's a... <strong>{{ inputValue }}</strong> ? </h3>  
@@ -40,7 +45,9 @@ export default {
         return {
             inputValue: '',
             animalList: animalsArray,
-            unknown: unknown
+            unknown: unknown,
+            animate: false,
+            animationCount: 3
         };
     },
     methods: {
@@ -74,7 +81,29 @@ export default {
             else {
                 console.log('Error! Input is empty');
             }
+        },
+        getRandomInt: function(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        },
+        shakeTheEgg: function() {
+            this.animate = true;
+            console.log("I'm shaking " + this.animationCount + " times");
+            setTimeout(() => { 
+                this.animate = false; 
+                this.animationCount = this.getRandomInt(1,5);
+                this.$refs.eggcontainer.style.setProperty('--ani-iteration-count', this.animationCount);
+                this.shakeTimer(this.getRandomInt(0,6), this.shakeTheEgg) 
+                } , 1500);
+        },
+        shakeTimer: function(delay, e) {
+            setTimeout(function(){
+                e();
+                console.log( "It took (mseconds): " + (delay * 1000 + 1500) );
+            }, delay * 1000);
         }
+    },
+    mounted() {
+        this.shakeTimer(this.getRandomInt(2,5), this.shakeTheEgg);
     }
 }
 </script>
@@ -85,10 +114,38 @@ export default {
 <style lang="scss" scoped>
 @import '~assets/css/variables';
 
+#eggcontainer {
+    --ani-iteration-count: 3;
+}
+
+// Animations
+@keyframes eggShake1 {
+    0% { transform:rotate(0deg) skew(0deg); }
+    33% { transform:rotate(-5deg) skewX(3deg); }
+    66% { transform:rotate(5deg) skewX(-3deg); }
+    100% { transform:rotate(0deg) skew(0deg); }
+}
+
+@keyframes eggShake2 {
+    0% { transform:rotate(0deg) skew(0deg); }
+    33% { transform:rotate(5deg) skewY(3deg); }
+    66% { transform:rotate(-5deg) skewX(-3deg); }
+    100% { transform:rotate(0deg) skew(0deg); }
+}
+
 .ini-egg {
     // max-height: 30vh;
-    max-height: clamp(25vh, 30vw, 30vh);
-    max-width: 100%;
+    height: clamp(25vh, 30vw, 30vh);
+    width: auto;
+    transition: all ease .2s;
+    transform-origin: 50% 80%;
+    // border: 1px solid red;
+}
+
+.animateEgg {
+    animation-name: eggShake2;
+    animation-duration: 250ms;
+    animation-iteration-count: var(--ani-iteration-count, 3);
 }
 
 .input-field {
